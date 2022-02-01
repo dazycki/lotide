182885de80c4c650102d8fc8e4cd29464637b3b0
Daryan Zycki <da.zycki@gmail.com>
2022-02-01 04:58:00 +0000

    Created a function to compare 2 arrays and save the difference in a new array.

---
+++ without.js
@@ -0,0 +1,39 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const without = function (source, itemsToRemove) {
+  let arrayDifference = source.filter(x => !itemsToRemove.includes(x));
+  return arrayDifference
+};
+
+//Test Cases
+assertArraysEqual((without([1, 2, 3], [1])), [2, 3]); //Pass
+assertArraysEqual((without(["1", "2", "3"], [1, 2, "3"])), ["1", "2"]); //Pass
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this 
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]); //Pass
