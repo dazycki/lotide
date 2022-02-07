9876e29ec6cb007d2636ffb1141eacfd74e06398
Daryan Zycki <da.zycki@gmail.com>
2022-02-07 04:19:21 +0000

    Created a function to push index position to array stored in object.

---
+++ letterPositions.js
@@ -0,0 +1,51 @@
+// FUNCTION IMPLEMENTATION ASSERT ARRAYS EQUALS
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+// FUNCTION LETTER POSITIONS
+const letterPositions = function (sentence) {
+  const myArray = sentence.split("");
+  const results = {};
+  for (const [index, value] of myArray.entries()) {
+    if (value !== " ") {
+      if (results[value]) {
+        results[value].push(index);
+      } else {
+        results[value] = [index];
+      }
+    }
+  }
+  return results;
+};
+
+//TEST CASES
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
+
+assertArraysEqual(letterPositions("lighthouse in the house").l, [0]);
+assertArraysEqual(letterPositions("lighthouse in the house").i, [1, 11]);
+assertArraysEqual(letterPositions("lighthouse in the house").g, [2]);
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
+assertArraysEqual(letterPositions("lighthouse in the house").t, [4, 14]);
+assertArraysEqual(letterPositions("lighthouse in the house").o, [6, 19]);
+assertArraysEqual(letterPositions("lighthouse in the house").u, [7, 20]);
+assertArraysEqual(letterPositions("lighthouse in the house").s, [8, 21]);
+assertArraysEqual(letterPositions("lighthouse in the house").e, [9, 16, 22]);
+assertArraysEqual(letterPositions("lighthouse in the house").n, [12]);
