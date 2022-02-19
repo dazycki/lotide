eb2252a13c8d367bb13935ccae7628e57da522bb
Daryan Zycki <da.zycki@gmail.com>
2022-02-19 06:14:37 +0000

    Refactor to seperate tests from function declaration file

--- head.js
+++ head.js
@@ -1,16 +1,9 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
+const assertEqual = require('./assertEqual'); //Define assertEqual
+
+// FUNCTION IMPLEMENTATION FOR HEAD 
+const head = function (array) { //Function takes in an array
+  return array[0]; //Fuction will return first item from array
 };
 
-const head = function (array) {
-  return array[0];
-};
-
-//Test cases for head function
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
+//EXPORT FUNCTION FOR USE IN OTHER FILES
+module.exports = head;
