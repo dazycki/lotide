41e5a4d80d09600f0986a1771a62eed2430abbbd
Daryan Zycki <da.zycki@gmail.com>
2022-02-20 04:17:21 +0000

    Refactor to export test cases.

--- eqObjects.js
+++ eqObjects.js
@@ -1,53 +1,26 @@
-// FUNCTION IMPLEMENTATION ASSERT EQUAL
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// FUNCTION IMPLEMENTATION EQUAL ARRAYS
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays'); //"Pull in" eqArrays function
 
 // FUNCTION IMPLEMENTATION EQUAL OBJECTS
-const eqObjects = function(object1, object2) {
-  let objKeys1 = Object.keys(object1);
-  objKeys1.sort();
-  let objKeys2 = Object.keys(object2);
-  objKeys2.sort();
-  if (objKeys1.length !== objKeys2.length) {
+const eqObjects = function(object1, object2) {//Takes in 2 objects
+  let objKeys1 = Object.keys(object1); //Creates array of object1's keys
+  objKeys1.sort(); //Sorts array in ascending order
+  let objKeys2 = Object.keys(object2); //Creates array of object2's keys
+  objKeys2.sort(); //Sorts array in ascending order
+  if (objKeys1.length !== objKeys2.length) { //Checks if both objects have same number of keys
     return false;
   } else {
-    for (const value of objKeys1) {
-      if (Array.isArray(objKeys1[value])) {
-        if (!(eqArrays(object1[value], object2[value]))) {
+    for (const value of objKeys1) { //Loops through object1
+      if (Array.isArray(objKeys1[value])) { //Checks if key is storing array
+        if (!(eqArrays(object1[value], object2[value]))) {//Checks if object1 and object2's arrays are equal for a given key-value pair
           return false;
         }
-      } else if (objKeys1[value] !== objKeys2[value]) {
+      } else if (objKeys1[value] !== objKeys2[value]) { //Checks if object1 and object2's other key-value pairs are equal
         return false;
       }
     }
   }
-  return true;
+  return true; //If no checks fail true will be returned
 };
 
 //EXPORT FUNCTION FOR USE IN OTHER FILES
 module.exports = eqObjects;
-
-
-//TEST CASES
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
