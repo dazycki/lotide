9473641bb199ef7d9be8e631a84c2e6444c61be7
Daryan Zycki <da.zycki@gmail.com>
2022-02-20 01:39:11 +0000

    Refactor to export assertArraysEqual function and require eqArrays.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,13 @@
+const eqArrays = require('./eqArrays'); //"Pull in" eqArrays function
+
 // FUNCTION IMPLEMENTATION ASSERT ARRAYS EQUALS
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-      return false;
-    } else {
-      console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-      return true;
-    }
+const assertArraysEqual = function (arr1, arr2) { //Takes in 2 arrays
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`); //Return if arrays are equal
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`); //Return if arrays are NOT equal
   }
 };
 
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
+//EXPORT FUNCTION FOR USE IN OTHER FILES
+module.exports = assertArraysEqual;
