68cd3ef22e08dac71236e70f3f872f37a2845f10
Daryan Zycki <da.zycki@gmail.com>
2022-02-20 01:46:30 +0000

    Refactor to seperate tests from function declaration file and added comments.

--- middle.js
+++ middle.js
@@ -1,51 +1,16 @@
-// FUNCTION IMPLEMENTATION ASSERT ARRAYS EQUALS
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2) === true) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-// IMPLEMENT MIDDLE FUNCTION
-const middle = function (arr) {
+// FUNCTION IMPLEMENTATION FOR MIDDLE 
+const middle = function (arr) { //Takes in an array
   let newArr = []
-  if (arr.length <= 2) {
+  if (arr.length <= 2) { //Returns an empty array for arrays with 1 or 2 elements
     return newArr;
   } else if (arr.length % 2 === 1) {
-    newArr.push(arr[Math.floor((arr.length) / 2)]);
+    newArr.push(arr[Math.floor((arr.length) / 2)]); //Returns 1 value if array has odd number of elements
   } else if (arr.length % 2 === 0) {
-    newArr.push(arr[Math.floor((arr.length - 1) / 2)]);
-    newArr.push(arr[Math.floor((arr.length) / 2)]);
+    newArr.push(arr[Math.floor((arr.length - 1) / 2)]); 
+    newArr.push(arr[Math.floor((arr.length) / 2)]); //Returns 2 values if array has even number of elements
   }
   return newArr;
 }
 
-//TEST CASES 
-// middle([1]) // => []
-assertArraysEqual(middle([1]), []);
-
-// middle([1, 2]) // => []
-assertArraysEqual(middle([1, 2]), []);
-
-// middle([1, 2, 3]) // => [2]
-assertArraysEqual(middle([1, 2, 3]), [2]);
-
-// middle([1, 2, 3, 4, 5]) // => [3]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-// middle([1, 2, 3, 4]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-
-// middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+//EXPORT FUNCTION FOR USE IN OTHER FILES
+module.exports = middle;
