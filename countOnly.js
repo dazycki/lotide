c89fb4ebe2e81ca701df83c3e8f27b0c3ee481f1
Daryan Zycki <da.zycki@gmail.com>
2022-02-20 03:42:40 +0000

    Refactor to export tests.

--- countOnly.js
+++ countOnly.js
@@ -1,29 +1,15 @@
-// FUNCTION IMPLEMENTATION ASSERT EQUAL
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // FUNCTION IMPLEMENTATION COUNT ONLY
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
-const countOnly = function (allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
-  for (const item of allItems) {
-    console.log(item);
-    // inside the loop,
-    // increment the counter for each item:
-    //   set a property with that string key to:
-    //     the value that was already there (or zero if nothing there) with 1 added to it.
+  for (const item of allItems) { //loops through keys within object
     if (itemsToCount[item]) {
       if (results[item]) {
-        results[item] += 1;
+        results[item] += 1; //increments counter for property if already there
       } else {
-        results[item] = 1;
+        results[item] = 1; //sets a property with string key if not already there
       }
     }
   }
@@ -33,23 +19,3 @@ const countOnly = function (allItems, itemsToCount) {
 
 //EXPORT FUNCTION FOR USE IN OTHER FILES
 module.exports = countOnly;
-
-//TEST CODE
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
