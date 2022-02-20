13d52614c36397f10d3822ae3d01da4f7a5ee19e
Daryan Zycki <da.zycki@gmail.com>
2022-02-20 01:20:41 +0000

    Created seperate file  for test cases related to head  function.

--- test/headTest.js
+++ test/headTest.js
@@ -2,6 +2,6 @@ const head = require('../head'); //"Pull in" head function
 const assertEqual = require('../assertEqual'); //"Pull in" assertEqual function
 
 //Test cases for head function
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
+assertEqual(head([5, 6, 7]), 5); //Should return true/ pass
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //Should return true/ pass
+assertEqual(head([1]), 1); //Should return true/ pass
