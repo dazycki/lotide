09ff27e1107144ea846eb00b8f2e336c0eb1bdef
Daryan Zycki <da.zycki@gmail.com>
2022-02-20 01:24:10 +0000

    Added inline comments to explain code.

--- tail.js
+++ tail.js
@@ -2,11 +2,11 @@ const assertEqual = require('./assertEqual'); //"Pull in" assertEqual function
 
 // FUNCTION IMPLEMENTATION FOR TAIL 
 const tail = function (array) { //Function takes in an array
-  let modArray = [];
+  let modArray = []; //Results will be pushed to empty array
   for (let i = 1; i < array.length; i++) {
-    modArray.push(array[i]);
+    modArray.push(array[i]); //Push items to new array starting at the second element
   }
-  return modArray;
+  return modArray; //Returns new array
 };
 
 //EXPORT FUNCTION FOR USE IN OTHER FILES
