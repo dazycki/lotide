0b55f2a73669fa0876e59a0e8000f08687a12ce1
Daryan Zycki <da.zycki@gmail.com>
2022-02-20 01:31:59 +0000

    Refactor to seperate tests from function declaration file and added comments.

--- eqArrays.js
+++ eqArrays.js
@@ -1,22 +1,14 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual'); //"Pull in" assertEqual function
 
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
+const eqArrays = function (arr1, arr2) { //Takes in 2 arrays
+  if (arr1.length !== arr2.length) return false; //Checks if both arrays are the same length
+  for (let i = 0; i < arr1.length; i++) { //Checks that both arrays have identical items
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
-  return true;
+  return true; //If all conditions pass will return true
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays(["Dave", 1, "Bob"], ["Dave", 1, "Bob"]), true); // => should PASS
-assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), true); // => should FAIL
-assertEqual(eqArrays([1, 2, "3"], ["3", 2, 1]), true); // => should FAIL
+//EXPORT FUNCTION FOR USE IN OTHER FILES
+module.exports = eqArrays;
