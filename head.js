9cacb8504a9d6eb9cb08954d1b110c0f1277868e
Daryan Zycki <da.zycki@gmail.com>
2022-02-20 01:25:01 +0000

    Refactor to remove unnecassary code.

--- head.js
+++ head.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual'); //Define assertEqual
-
 // FUNCTION IMPLEMENTATION FOR HEAD 
 const head = function (array) { //Function takes in an array
   return array[0]; //Fuction will return first item from array
