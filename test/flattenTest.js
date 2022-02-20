c84be1714b267eb4633445e29e3e37f3be3902a9
Daryan Zycki <da.zycki@gmail.com>
2022-02-20 03:27:07 +0000

    git commit -m

---
+++ test/flattenTest.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+const flatten = require('../flatten'); //"Pull in" flatten function
+
+//Test cases for flatten function
+describe("#flatten", () => {
+  it("returns [1, 2, 3, 4, 5, 6] for [1, 2, [3, 4], 5, [6]]", () => {
+    assert.deepEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+  });
+  it("returns ['abc', 'efg', 'hij', 'bob'] for ['abc', ['efg', 'hij'], 'bob']", () => {
+    assert.deepEqual(flatten(['abc', ['efg', 'hij'], 'bob']), ['abc', 'efg', 'hij', 'bob']);
+  });
+});
